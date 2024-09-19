68e7ed6e1224c2b19a89cdc161b01db055236845
Denise Chang <ddleletan@hotmail.com>
2024-09-19 17:25:04 -0400

    simulate data

--- scripts/simulation.R
+++ scripts/simulation.R
@@ -11,11 +11,30 @@
 #### Workspace setup ####
 library(tidyverse)
 
+
 #### Simulate data ####
-date <-
+set.seed(304)
+
+# Define the start and end date
+start_date <- as.Date("2018-01-01")
+end_date <- as.Date("2023-12-31")
+
+# Set the number of random dates you want to generate
+number_of_dates <- 100
+
+data <-
   tibble(
-    date = as.Date(runif),
-    number_of_marriages = rpois(n = 100, lambda = 15)
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+      origin = "1970-01-01"
+    ),
+    number_of_marriage = rpois(n = number_of_dates, lambda = 15)
   )
 
- 
+
+#### Write_csv
+write_csv(data, file = "data/raw_data/simulated.csv")
